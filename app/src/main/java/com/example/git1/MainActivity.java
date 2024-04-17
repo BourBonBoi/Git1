6821f841699aaf339dcf0b95c30e194d60bf0d96
BourBonBoi <artemmine9292@gmail.com>
2024-04-17 14:27:56 +0900

    add pr4

--- app/src/main/java/com/example/git1/MainActivity.java
+++ app/src/main/java/com/example/git1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.git1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -139,6 +140,51 @@ public class MainActivity extends AppCompatActivity {
                              mTextView.setText("Что-то пошло не так: " + throwable.getMessage());
                          }
                      }
+
         );
+    }
+    public void onClickPr4(View view) {
+        mProgressBar.setVisibility(View.VISIBLE);
+        mTextView.setText("");
+        mTextViewSecond.setText("");
+
+        GitHubServicePr4 gitHubService = GitHubServicePr4.retrofit.create(GitHubServicePr4.class);
+        // часть слова
+        final Call<GitResult> call =
+                gitHubService.getUsers("alexanderklim");
+
+        call.enqueue(new Callback<GitResult>() {
+            @Override
+            public void onResponse(Call<GitResult> call, Response<GitResult> response) {
+                // response.isSuccessful() is true if the response code is 2xx
+                if (response.isSuccessful()) {
+                    GitResult result = response.body();
+
+                    // Получаем json из github-сервера и конвертируем его в удобный вид
+                    // Покажем только первого пользователя
+                    String user = "Аккаунт Github: " + result.getItems().get(0).getLogin();
+                    mTextView.setText(user);
+                    Log.i("Git", String.valueOf(result.getItems().size()));
+
+                    mProgressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    int statusCode = response.code();
+
+                    // handle request errors yourself
+                    ResponseBody errorBody = response.errorBody();
+                    try {
+                        mTextView.setText(errorBody.string());
+                        mProgressBar.setVisibility(View.INVISIBLE);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GitResult> call, Throwable throwable) {
+                mTextView.setText("Что-то пошло не так: " + throwable.getMessage());
+            }
+        });
     }
 }
