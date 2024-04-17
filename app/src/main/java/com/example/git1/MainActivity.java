71df03e8c8dc228780de07842bf0399cb5656399
BourBonBoi <artemmine9292@gmail.com>
2024-04-17 14:23:47 +0900

    add pr3

--- app/src/main/java/com/example/git1/MainActivity.java
+++ app/src/main/java/com/example/git1/MainActivity.java
@@ -100,4 +100,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void onClickPr3(View view) {
+        mTextView.setText("");
+        mTextViewSecond.setText("");
+        mProgressBar.setVisibility(View.VISIBLE);
+        GitHubService gitHubService = GitHubService.retrofit.create(GitHubService.class);
+
+        final Call<List<Repos>> call = gitHubService.getRepos("alexanderklimov");
+
+        call.enqueue(new Callback<List<Repos>>() {
+                         @Override
+                         public void onResponse(Call<List<Repos>> call, Response<List<Repos>> response) {
+                             // response.isSuccessfull() is true if the response code is 2xx
+                             if (response.isSuccessful()) {
+                                 // Выводим массив имён
+                                 mTextView.setText(response.body().toString() + "\n");
+                                 for (int i = 0; i < response.body().size(); i++) {
+                                     // Выводим имена по отдельности
+                                     mTextView.append(response.body().get(i).getName() + "\n");
+                                 }
+
+                                 mProgressBar.setVisibility(View.INVISIBLE);
+                             } else {
+                                 int statusCode = response.code();
+                                 // Обрабатываем ошибку
+                                 ResponseBody errorBody = response.errorBody();
+                                 try {
+                                     mTextView.setText(errorBody.string());
+                                     mProgressBar.setVisibility(View.INVISIBLE);
+                                 } catch (IOException e) {
+                                     e.printStackTrace();
+                                 }
+                             }
+                         }
+
+                         @Override
+                         public void onFailure(Call<List<Repos>> call, Throwable throwable) {
+                             mTextView.setText("Что-то пошло не так: " + throwable.getMessage());
+                         }
+                     }
+        );
+    }
 }
